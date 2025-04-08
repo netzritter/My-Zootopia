7c0605cc5cf16dffc260b8ff0922cb6c01c51a35
unknown <nicolasheyer@gmail.com>
2025-04-08 20:28:54 +0200

    bring animal data into html

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -1,36 +1,59 @@
 import json
 
 def load_data(file_path):
-    """Load as JSON file"""
+    """Load a JSON file."""
     with open(file_path, "r", encoding="utf-8") as handle:
         return json.load(handle)
 
 
-animals = load_data("animals_data.json")
+def read_html(file_path):
+    """Read HTML template as a string."""
+    try:
+        with open(file_path, "r", encoding="utf-8") as file_obj:
+            return file_obj.read()
+    except FileNotFoundError:
+        print(f"Error: File '{file_path}' not found.")
+        return ""
 
 
-def print_specific_animal_data(animals_db):
+def generate_animal_summary(animals):
+    """Generate a string with animal information."""
+    animal_info = ""
     for animal in animals:
-        if "name" in animal:
-            print("Name:", animal["name"].upper())
+        animal_info += f"Name: {animal['name']}\n"
+        animal_info += f"Diet: {animal.get('characteristics', {}).get('diet', 'Unknown')}\n"
+        animal_info += f"Location: {animal['locations'][0]}\n"
+        animal_info += f"Type: {animal.get('characteristics', {}).get('type', 'Unknown')}\n\n"
+    return animal_info
 
-        diet = animal.get("characteristics", {}).get("diet")
-        if diet:
-            print("Diet:", diet)
 
-        # Print first location and check if locations exist
-        locations = animal.get("locations")
-        if locations and len(locations) > 0:
-            print("Location:", locations[0])
+def create_final_html(animals):
+    """Replace placeholder with animal info and return final HTML string."""
+    template = read_html("animals_template.html")
+    animal_data_string = generate_animal_summary(animals)
 
-        animal_type = animal.get("characteristics", {}).get("type")
-        if animal_type:
-            print("Type:", animal_type.capitalize())
-        print()
+    if template is None or animal_data_string is None:
+        raise ValueError("Template or animal data string is None")
+
+    final_html = template.replace("__REPLACE_ANIMALS_INFO__", animal_data_string)
+
+    return final_html
+
+
+def save_final_html_template(combined_content):
+    with open("animals.html", "w", encoding="utf-8") as output_file:
+        output_file.write(combined_content)
+        print("HTML output successfully written to animals.html")
+
 
 def main():
+    # Load the animal data and the HTML template
+    animals = load_data("animals_data.json")
+    final_html = create_final_html(animals)
 
-    print_specific_animal_data(animals)
+    # Write the combined content to the "animals.html" file
+    save_final_html_template(final_html)
+
 
 if __name__ == "__main__":
     main()
